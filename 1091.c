5a20a8542e0d5e9d5fa75aaebad8a56f831ca2a2
yazhoupromise <43847573+yazhoupromise@users.noreply.github.com>
2020-06-26 16:41:39 +0800

    Add files via upload

---
+++ 1091.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    int a;
+    int b;
+    while ((scanf("%d%d",&a,&b)!=EOF))
+    {
+       if((a+b!=0))
+           printf("%d\n",a+b);
+       
+else 
+return  0;
+    }
+    
+}
